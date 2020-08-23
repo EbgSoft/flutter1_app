a3e685dd582d96b800811be54c27a424f22823f6
Murat G <muratg@Murats-MacBook-Pro.local>
2020-08-23 19:07:57 +0300

    Initial commit

---
+++ lib/styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const buttonText = TextStyle(
+    color: Color.fromRGBO(0, 0, 0, 0.8),
+    //fontFamily: 'Tahoma',
+    fontSize: 16.0,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const sonucText = TextStyle(
+    color: Colors.red,
+    //fontFamily: 'Tahoma',
+    fontSize: 26,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+  );
+}
---
+++ readme.txt
@@ -0,0 +1,18 @@
+
+Flutter Ders-2
+
+Konu : Basit bir UI ile iki adet input alan ve aritmetik toplama işlemi yaparak
+ sonucu ekranda gösteren bir basit hesap makinesi uygulaması oluşturası.
+
+1-Stattefull bir widget oluşturma
+2-UI oluşturma
+3-Değişkenle tanımlama
+4-Aritmetik İşlmeleri bir fonksiyona yaptırma.
+5-Style Tanımlama ve kullama
+6-Input Contoller tanımlama
+
+Yeni Kullanılacak Widgetlar
+
+1-TextFormField
+2-RaisedButton
+3-SizedBox
