f7cc0fb7fbbba158fe4b6daa9003e2da0d136272
ebgsoft <veriverinsan@gmail.com>
2020-09-18 22:15:09 +0300

    Flutter 6

--- lib/flutterDers5/main.dart
+++ lib/flutterDers5/main.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+
+void main() {
   runApp(MyFlutter5());
 }
 
@@ -21,84 +21,104 @@ class MyPage extends StatefulWidget {
 }
 
 class _MyPageState extends State<MyPage> {
-  String mesaj="Button Durum";
+
+  String mesaj="Button Durumu";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text("Flutter Ders 5"),
-        ),
-        body:
-        Container(
-          padding:EdgeInsets.only(top: 100.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  RaisedButton(
-                    child: Text("My Button"),
-                    onPressed: (){
-                      setState(() {
-                        mesaj="RaisedButton Basıldı.";
-                        debugPrint(mesaj);
-                      });
-                    },
+      appBar: AppBar(
+        title: Text("Flutter Ders 5"),
+
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 50),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //bir row yapalım ve buraya buttonları koyalım
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RaisedButton(
+                  child: Text("Benim Butonum"
+                    ,style: TextStyle(fontSize: 15),),
+                  onPressed: () {
+                    setState(() {
+                      mesaj="RaisedButona basıldı";
+                    });
+                    debugPrint(mesaj);
+                  },
+                  color: Colors.blue,
+                ),
+                FlatButton(
+                  child: Icon(
+                    Icons.star,color: Colors.orange,
                   ),
-                  FlatButton(
-                    child: Icon(Icons.thumb_up,color: Colors.blueGrey,),
-                    color: Colors.transparent,
-                    hoverColor: Colors.green,
-                    onPressed: (){
-                      setState(() {
-                        mesaj="FlatButton Basıldı.";
-                        debugPrint(mesaj);
-                      });
-                    },
+                  hoverColor: Colors.green,
+                  onPressed: () {
+                    setState(() {
+                      mesaj="FlatButona basıldı";
+                    });
+                  },
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.thumb_up,color: Colors.blue,
                   ),
-                  IconButton(icon: Icon(Icons.person),
-                      color: Colors.orange,
-                      onPressed: (){
-                        setState(() {
-                          mesaj="IconButton  Basıldı.";
-                          debugPrint(mesaj);
-                        });
-                      }
-                  ),
-                  GestureDetector(
-                    child: Image(image: AssetImage("assets/images/elma.jpg"),width: 100,height: 100, ),
-                    onTap: () {
-                      setState(() {
-                        mesaj="Image Üzerine Basıldı.";
-                        debugPrint(mesaj);
-                      });
-                    },
-                    onDoubleTap: () {
-                      setState(() {
-                        mesaj="Image Üzerine çift tıklandı";
-                        debugPrint(mesaj);
-                      });
-                    },
-                  ),
-                ],
+                  onPressed: () {
+                    setState(() {
+                      mesaj="IconButtona basıldı";
+                    });
+                  },
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      mesaj="Elma üzerine tıklandı";
+                    });
+                  },
+                  child: Column(
+                    children: [
+                      Text("Bu Bir Elma"),
+                      Image(
+                        image: AssetImage("assets/images/elma.jpg"),
+                        width: 100,
+                        height: 100,
+                      )
+                    ],
+                  ) ,
+                )
+              ],
+            ),
+            //boşuk bırakalım
+            SizedBox(height: 50,)
+            //bir text output yapalım
+            ,Text(mesaj,style: TextStyle(fontSize: 22,color: Colors.orange),)
+            ,SizedBox(height: 50,)
+            //image gösterelim
+            ,GestureDetector(
+              onTap: () {
+                setState(() {
+                  mesaj="Resim üzerine tıklandı";
+                });
+              },
+              onDoubleTap: () {
+                setState(() {
+                  mesaj="Resim üzerinde çift tıklandı";
+                });
+              },
+              child:     Image(
+                image: AssetImage("assets/images/flutter-5-1.png"),
+                width: 350,
+                height: 200,
               ),
-              SizedBox(height: 100,),
-              Text(mesaj, style: TextStyle(
-                fontSize: 22,
-                color: Colors.orange
-              ),),
-              Image(image: AssetImage("assets/images/flutter-5.png"),
-                  width: 300, height: 250),
-            ],
-          ),
+            )
 
-        )
-
+          ],
+        ),
+      ),
     );
   }
 }
-
---
+++ lib/flutterDers6/main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
+
+class BekletenIslem {
+
+  Future<String> _beklenenIslemAsync() async {
+
+    return Future.delayed(const Duration(seconds: 5), () => "Bitti");
+
+  }
+
+  bool _beklenenIslemSync()
+  {
+    sleep(Duration(seconds:5));
+  }
+
+  Future<String> _getIPAddress() async {
+    final url = 'https://httpbin.org/ip';
+    final response = await http.get(url);
+    String ip = jsonDecode(response.body)['origin'];
+    return ip;
+  }
+}
+
+
+void main() {
+  runApp(MyFlutter6());
+}
+
+class MyFlutter6 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Ders 6",
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatefulWidget {
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  String ipAdress="";
+
+  bool bittiMi=false;
+
+  @override
+  void initState() {
+    _controller = AnimationController(vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset("assets/images/ebgsoftlogo.png",width: 100, height: 50,),
+            Text("Flutter 6")
+          ],
+        )
+      ),
+      body: Center(
+        child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           crossAxisAlignment: CrossAxisAlignment.center,
+           children: [
+             Text("Future, Async, Sync",style: TextStyle(fontSize: 33),),
+             SizedBox(height: 45.0,),
+             Text("Bittimi: " + bittiMi.toString(),style: TextStyle(fontSize: 22,
+                 backgroundColor: bittiMi? Colors.green: Colors.red),),
+             SizedBox(height: 15.0,),
+             Text("IPAdress: " + ipAdress,style: TextStyle(fontSize: 22,
+              backgroundColor: bittiMi? Colors.green: Colors.red),),
+             SizedBox(height: 15.0,),
+             RaisedButton(
+               color: Colors.blue,
+               child: Text("Bekleten Islem Async",style: TextStyle(fontSize: 18),),
+               onPressed: () async {
+                 final bekleten = new BekletenIslem();
+                 setState(() {
+                   bittiMi=false;
+                 });
+                 try {
+                   bittiMi = await bekleten._beklenenIslemAsync()!=null;
+                    setState(() {
+
+                    });
+                   print(bittiMi);
+                 } catch (error) {
+                   print(error);
+                 }
+               },
+             ),
+             RaisedButton(
+               color: Colors.blue,
+               child: Text("Bekleten Islem Sync",style: TextStyle(fontSize: 18),),
+               onPressed: () async {
+                 final bekleten = new BekletenIslem();
+                 setState(() {
+                   bittiMi=false;
+                 });
+                 try {
+                   bittiMi = await bekleten._beklenenIslemSync()!=null;
+                   setState(() {
+                     bittiMi=true;
+                   });
+                   print(bittiMi);
+                 } catch (error) {
+                   print(error);
+                 }
+               },
+             ),
+             RaisedButton(
+               color: Colors.blue,
+               child: Text("What is My IP",style: TextStyle(fontSize: 18),),
+               onPressed: () async {
+                 final example1 = new BekletenIslem();
+                 try {
+                   ipAdress = await example1._getIPAddress();
+                   setState(() {
+
+                   });
+                   print(ipAdress);
+                 } catch (error) {
+                   print(error);
+                 }
+               },
+             ),
+           ],
+        ),
+      ),
+    );
+  }
+}
